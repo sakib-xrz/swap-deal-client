56aa16e8c48386da7c4825195c15eb099305215b
sakib-xrz <sakibxrz21@gmail.com>
2022-11-26 04:35:07 +0600

    Add a product form design done

--- src/Page/Dashboard/AddProduct.jsx
+++ src/Page/Dashboard/AddProduct.jsx
@@ -4,12 +4,32 @@ const AddProduct = () => {
   return (
     <div className="py-10 px-5">
       <section class="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md">
-        <h2 class="text-lg font-semibold text-gray-700 capitalize">
+        <h2 class="text-xl font-semibold text-gray-700 capitalize">
           Add A Product
         </h2>
 
         <form>
           <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+              <label class="text-gray-700" for="product-name">
+                Select a brand
+              </label>
+              <select
+                required
+                className="select block w-full h-[2.70rem] min-h-[2.70rem] px-4 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-0 focus:ring-0"
+              >
+                <option disabled selected>
+                  Brand
+                </option>
+                <option>Acer</option>
+                <option>Asus</option>
+                <option>Dell</option>
+                <option>Hp</option>
+                <option>Lenovo</option>
+                <option>Samsung</option>
+              </select>
+            </div>
+
             <div>
               <label class="text-gray-700" for="product-name">
                 Product name
@@ -23,26 +43,15 @@ const AddProduct = () => {
             </div>
 
             <div>
-              <label class="text-gray-700" for="price">
-                Price
+              <label htmlFor="file" className="text-gray-700">
+                Upload a image
               </label>
               <input
-                name="price"
-                id="price"
-                type="email"
-                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40"
-              />
-            </div>
-
-            <div>
-              <label class="text-gray-700" for="phone">
-                Phone
-              </label>
-              <input
-                name="phone"
-                id="phone"
-                type="text"
-                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40"
+                id="file"
+                type="file"
+                name="image"
+                accept="image/*"
+                className="file-input file-input-ghost w-full h-[2.70rem] min-h-[2.70rem] mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-0 focus:ring-0"
               />
             </div>
 
@@ -59,9 +68,72 @@ const AddProduct = () => {
             </div>
 
             <div>
+              <label class="text-gray-700" for="resale-price">
+                Selling price
+              </label>
+              <input
+                name="resale-price"
+                id="resale-price"
+                type="text"
+                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40"
+              />
+            </div>
+
+            <div>
+              <label class="text-gray-700" for="original-price">
+                Buying price
+              </label>
+              <input
+                name="original-price"
+                id="original-price"
+                type="text"
+                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40"
+              />
+            </div>
+
+            <div>
+              <label class="text-gray-700" for="use">
+                Used time
+              </label>
+              <input
+                name="use"
+                id="use"
+                type="text"
+                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40"
+              />
+            </div>
+
+            <div>
+              <label class="text-gray-700" for="name">
+                Seller name
+              </label>
+              <input
+                name="name"
+                id="name"
+                type="text"
+                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40"
+              />
+            </div>
+
+            <div>
+              <label class="text-gray-700" for="phone">
+                Phone
+              </label>
+              <input
+                name="phone"
+                id="phone"
+                type="tel"
+                class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40"
+              />
+            </div>
+
+            <div>
+              <label class="text-gray-700" for="product-name">
+                Select condition
+              </label>
               <select
                 required
-                className="select block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-0 focus:ring-0"
+                className="select block w-full h-[2.70rem] min-h-[2.70rem] px-4 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-0 focus:ring-0"
               >
                 <option disabled selected>
                   Condition
@@ -71,53 +143,17 @@ const AddProduct = () => {
                 <option>Fair</option>
               </select>
             </div>
+          </div>
 
-            <div>
-              <select
-                required
-                className="select block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-0 focus:ring-0"
-              >
-                <option disabled selected>
-                  Brand
-                </option>
-                <option>Acer</option>
-                <option>Asus</option>
-                <option>Dell</option>
-                <option>Hp</option>
-                <option>Lenovo</option>
-                <option>Samsung</option>
-              </select>
-            </div>
-
-            <div>
-              <label class="text-gray-700" for="use">
-                Used
-              </label>
-              <input
-                name="use"
-                id="use"
-                type="number"
-                min="2000"
-                max="2099"
-                step="1"
-                class="select block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-0 focus:ring-0"
-              />
-            </div>
-
-            <div>
-              <label
-                htmlFor="file"
-                className="flex items-center mx-auto mt-6 text-center border bg-white rounded-md cursor-pointer"
-              >
-                <input
-                  id="file"
-                  type="file"
-                  name="image"
-                  accept="image/*"
-                  className="file-input file-input-ghost w-full bg-white"
-                />
-              </label>
-            </div>
+          <div className="mt-5">
+            <label class="text-gray-700" for="description">
+              Description
+            </label>
+            <textarea
+              name="description"
+              id="description"
+              class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40"
+            />
           </div>
 
           <div class="flex justify-end mt-6">
